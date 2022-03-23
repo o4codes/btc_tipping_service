d0731cbdf53168310448b87bf8a780fea27a6449
shadrach <oforkansi.shadrach@gmail.com>
2022-03-23 21:19:20 +0100

    utils sendpayment function added

--- bitnob_tip/utils/bitnob_handler.py
+++ bitnob_tip/utils/bitnob_handler.py
@@ -1,29 +1,38 @@
+from urllib.error import HTTPError
 import requests
 from decouple import config
 
-class BitnobHandler:
-    """ class handles all requests to the Bitnob API
-    """
+from .schemas import (
+    PaymentPriority,
+    BitnobCustomer,
+    BtcOnChainPayment,
+    BtcLightningPayment
+)
     
+class BitnobHandler:
+    """class handles all requests to the Bitnob API"""
+
     def __init__(self) -> None:
-        self.__base_url = 'https://sandboxapi.bitnob.co'
-        self.__customer_endpoint = '/api/v1/customers'
-        self.__secret_key = config('BITNOB_SECRET_KEY')
-        self.__public_key = config('BITNOB_PUBLIC_KEY')
-        
-    def create_customer(self, **customer_data) -> dict:
-        """ creates a customer in Bitnob
-        
+        self.__base_url = "https://sandboxapi.bitnob.co"
+        self.__customer_endpoint = "/api/v1/customers"
+        self.__onchain_btc_endpoint = "/api/v1/wallets/send_bitcoin"
+        self.__lightining_endpoint = "/api/v1/lnurl/paylnaddress"
+        self.__secret_key = config("BITNOB_SECRET_KEY")
+        self.__public_key = config("BITNOB_PUBLIC_KEY")
+
+    def create_customer(self, customer: BitnobCustomer) -> dict:
+        """creates a customer in Bitnob
+
         Args:
-            customer_data (dict): customer data to be created in Bitnob
-            sample_data: {
+            customer_data (BitnobCustomer): customer data to be created in Bitnob
+            sample_data: 
                 'firstName': 'John',
                 'lastName': 'Doe',
                 'email': 'johndoe@mail.com',
                 'phone': '1234567890',
                 'countryCode': '+234'
-            }
-        
+            
+
         Returns:
             dict: response from Bitnob
             sample_data: {
@@ -37,22 +46,109 @@ class BitnobHandler:
                 "createdAt": "2021-08-26T11:15:23.788Z",
                 "updatedAt": "2021-08-26T11:15:23.788Z"
             }
-            
+
         Raises:
             Exception: if request fails
+            Exception: if connection fails
         """
-        
+
         url = f"{self.__base_url}{self.__customer_endpoint}"
+
+        headers = {
+            "Authorization": f"Bearer {self.__secret_key}",
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        }
+        
+        data = customer.to_request_payload()
+        
+        try:
+            response = requests.post(url, json=data, headers=headers)
+
+            if response.status_code == 200:
+                return response.json()["data"]
+
+            raise Exception(f"Creation Error: {response.json()['message']}")
+
+        except (HTTPError, ConnectionError) as e:
+            raise Exception(f"Error creating customer: {e}")
+
+    def send_onchain_btc(self, payment_request: BtcOnChainPayment) -> dict: 
+        """sends onchain btc payment to Bitnob
+        
+        Args:
+            payment_request (BtcOnChainPayment): payment request to be sent to Bitnob
+            Sample data:
+                'satoshis': 100000000,
+                'address': '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX',
+                'customerEmail': 'mail@mail.com',
+                'description': 'test',
+                'priorityLevel': 'regular'
+                
+        Returns:
+            dict: response from Bitnob
+        
+        Raises:
+            Exception: if payment request fails
+            Exception: if connection fails
+        """
+        url = f"{self.__base_url}{self.__onchain_btc_endpoint}"
         
         headers = {
-            'Authorization': f'Bearer {self.__secret_key}',
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
+            "Authorization": f"Bearer {self.__secret_key}",
+            "Content-Type": "application/json",
+            "Accept": "application/json",
         }
-        response = requests.post(url, json=customer_data, headers=headers)
         
-        if response.status_code == 200:
-            return response.json()['data']
         
-        raise Exception(f"Error creating customer: {response.json()['message']}")
+        data = payment_request.to_reqeust_payload()
+        
+        try:
+            response = requests.post(url, json=data, headers=headers)
+
+            if response.status_code == 200:
+                return response.json()["data"]
+
+            raise Exception(f"Payment Failed: {response.json()['message']}")
+        except (HTTPError, ConnectionError) as e:
+            raise Exception(f"Error sending onchain btc: {e}")
+        
+    def send_lightning_payment(self, payment_request: BtcLightningPayment) -> dict:
+        """sends lightning payment to Bitnob
+
+        Args:
+            payment_request (PaymentPriority): payment request to be sent to Bitnob
+            Sample data:
+                'btc_amount': 100000000,
+                'customerEmail': 'mail@mail.com
+                'lnAddress:'sdsdsds',
+                'reference':'asasas'
+        
+        Returns: 
+            dict: response from Bitnob
+        
+        Raises:
+            Exception: if payment request fails
+            Exception: if connection fails
+        """
+        url = f"{self.__base_url}{self.__lightining_endpoint}"
+        
+        headers = {
+            "Authorization": f"Bearer {self.__secret_key}",
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        }
+        
+        data = payment_request.to_request_payload()
+        
+        try:
+            response = requests.post(url, json=data, headers=headers)
+
+            if response.status_code == 200:
+                return response.json()["data"]
+
+            raise Exception(f"Payment Failed: {response.json()['message']}")
+        except (HTTPError, ConnectionError) as e:
+            raise Exception(f"Error sending lightning payment: {e}")
+        
     
---
+++ bitnob_tip/utils/schemas.py
@@ -0,0 +1,87 @@
+from enum import Enum
+
+
+class PaymentPriority(str, Enum):
+    """Enum choices for payment priority"""
+
+    REGULAR = "regular"
+    HIGH = "high"
+
+
+class BitnobCustomer:
+    """Schema for customer data"""
+
+    def __init__(
+        self, firstName: str, lastName: str, email: str, phone: str, countryCode: str
+    ):
+        self.__firstName = firstName
+        self.__lastName = lastName
+        self.__email = email
+        self.__phone = phone
+        self.__countryCode = countryCode
+
+    def to_request_payload(self) -> dict:
+        """Return dict representation of customer data"""
+        return {
+            "firstName": self.__firstName,
+            "lastName": self.__lastName,
+            "email": self.__email,
+            "phone": self.__phone,
+            "countryCode": self.__countryCode,
+        }
+
+
+class BtcOnChainPayment:
+    """Schema for onchain btc payment
+    """
+
+    def __init__(
+        self,
+        btc_amount: float,
+        address: str,
+        customerEmail: str,
+        description: str,
+        priorityLevel: PaymentPriority = PaymentPriority.REGULAR,
+    ):  
+        self.__btc_amount = btc_amount
+        self.__address = address
+        self.__customerEmail = customerEmail
+        self.__description = description
+        self.__priorityLevel = priorityLevel
+
+    def to_reqeust_payload(self) -> dict:
+        """Return dict representation of onchain btc payment
+        """
+        return {
+            "satoshis": self.__btc_amount * 100000000,
+            "address": self.__address,
+            "customerEmail": self.__customerEmail,
+            "description": self.__description,
+            "priorityLevel": self.__priorityLevel,
+        }
+
+class BtcLightningPayment:
+    """Schema for lightning btc payment"""
+
+    def __init__(
+        self,
+        btc_amount: float, 
+        lnAddress: str,
+        reference: str,
+        customer_email: str,
+    ):
+        self.__btc_amount = btc_amount
+        self.__lnAddress = lnAddress
+        self.__reference = reference
+        self.__customer_email = customer_email
+        
+    def to_request_payload(self) -> dict:
+        """ Return dict representation of lightning btc payment
+        """
+        return {
+            "satoshis": self.__btc_amount * 100000000,
+            "lnAddress": self.__lnAddress,
+            "reference": self.__reference,
+            "customerEmail": self.__customer_email,
+        }
+       
