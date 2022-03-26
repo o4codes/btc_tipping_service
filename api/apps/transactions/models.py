17cb6ada0496dfaaab385a61e8d5e8bf42acddb4
shadrach <oforkansi.shadrach@gmail.com>
2022-03-26 14:49:24 +0100

    Onchain Adress Model createded

--- api/apps/transactions/models.py
+++ api/apps/transactions/models.py
@@ -5,6 +5,15 @@ from django.contrib.auth import get_user_model
 # Create your models here.
 
 
+class OnchainAddress(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.address
+
 class OnChainTransaction(models.Model):
     """
     Model for on-chain transactions.
