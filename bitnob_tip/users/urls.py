070774dc64e68b721f0a9aa7b2ace864ccaf2eaa
shadrach <oforkansi.shadrach@gmail.com>
2022-03-26 11:27:01 +0100

    delete api root folder

--- bitnob_tip/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from .views import UserCreateList, UserDetail
-
-urlpatterns = [
-    path("", UserCreateList.as_view(), name="user-create-list"),
-    path("<str:sec_id>", UserDetail.as_view(), name="user-detail"),
-]
