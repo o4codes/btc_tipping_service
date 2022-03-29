eb2cff7fd8d5f7f89e618bc3c6f0d6eccb87a777
shadrach <oforkansi.shadrach@gmail.com>
2022-03-29 16:35:20 +0100

    added environment variables for production

--- api/core/settings.py
+++ api/core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 
--- api/utils/bitnob_base.py
+++ api/utils/bitnob_base.py
@@ -2,7 +2,7 @@ from decouple import config
 
 class BitnobBase:
     def __init__(self):
-        self.base_url = "https://staging-api.flowertop.xyz"
+        self.base_url = config("BITNOB_STAGING_BASE_URL")
         self.secret_key = config("BITNOB_SECRET_KEY")
         self.headers = {
             "Authorization": f"Bearer {self.secret_key}",
